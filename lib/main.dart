9716340cefef28c7ff104a01c931492203581c4c
dhiastii <dhiastiputri.jayanti@gmail.com>
2023-05-14 20:35:59 +0700

    mengedit main.dart bagian home

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapi/view/kategoriBarang/add_kategori_barang.dart';
+import 'package:flutterapi/view/kategoriBarang/kategori_barang.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const AddKategoriBarang(),
+      home: const KategoriBarang(),
     );
   }
 }
